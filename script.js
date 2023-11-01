fff70bc0796cb4642c06491becc353b66fac7a26
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-11-01 13:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function volume_sphere() {
-  // Get the radius value from the input element and parse it as a float
+
   let radius = parseFloat(document.getElementById("radius").value);
 
-  // Check if the parsed radius is a valid number
   if (isNaN(radius) || radius <= 0) {
-    alert("Invalid radius. Please provide a positive number.");
+    document.getElementById("volume").value = "NaN";
     return; // Exit the function
   }
 
-  // Calculate the volume of the sphere
+
  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
 	const formattedVolume = volume.toFixed(4);
 
-  // Update the volume input field with the result
+
     document.getElementById("volume").value = formattedVolume;
 }
 
-// Set the form's onsubmit event handler to call volume_sphere
+
 document.getElementById('MyForm').onsubmit = function (event) {
-  event.preventDefault(); // Prevent the form from actually submitting
-  volume_sphere(); // Call your function
+  event.preventDefault(); 
+  volume_sphere(); 
 };
